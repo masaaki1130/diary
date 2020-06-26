3135c55957cc25947decf93f813d26e37fd90d26
masaaki1130 <masaki11maki30@gmail.com>
2020-06-26 14:43:34 +0900

    userfix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_095706) do
+ActiveRecord::Schema.define(version: 2020_06_26_054124) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2020_06_24_095706) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.string "comment"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
