bd66ee7030a5668a632df99f6f3a67c9b28b839a
masaaki1130 <masaki11maki30@gmail.com>
2020-07-13 02:15:08 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users, only: [:edit, :update, :show]
-  resources :messages, only: [:index, :create]
+  resources :messages, only: [:index, :create, :show]
 end
